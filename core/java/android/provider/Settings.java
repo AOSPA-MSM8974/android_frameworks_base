696532141d44099fc566cb7d09922923c3f5446f
Alex Cruz <du.alexcruz@gmail.com>
2019-01-13 10:59:49 -0600

    Bring back traffic indicator for all!
    arrow-edits:
     - adapt to Q
    
    Change-Id: Ia41474a262b258f8a2d4d7b10a15891729ffd17c
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    
    Add network traffic monitor properly to statusbar icons
    
    Make it behave fine on devices that don't have much status bar space
    (e.g. because of a display cutout/"notch")
    
    Arrow edits:- nuke from status_bar.xml
    
    Change-Id: Icbb2715ae78ce06bd3b79dc23ff491db202985f2
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    
    Option for Up, Down as well for Network Traffic [1/2]
    
    Change-Id: I2dabd04b91b16f72b30a913e4817b6ca90e9c34e
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    
    NetworkTraffic: Show arrows in statusbar [1/2]
    
    arrow-edits:
    adapt code to our dynamic arrows method
    
    SKULSHADY: Reverse logic of the toggle
    spezi77: Adapt for statusbar traffic indicator
    
    Change-Id: Ie50dbe3f54495d4b5c0c0a51969df595de7a1386
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    
    Network Traffic: Add Option for Users to select size of Single Traffic (Up/Down) Font Size
    
    Thanks to @SKULSHADY's commit - Havoc-OS/android_frameworks_base@20e6c63#diff-a6b14ee550885fae2c9316575fd6ce9c
    
    Change-Id: If9eef36de9ce6e70cc2b94c920295f30ea40751a
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    
    NetworkTraffic: Fix gravity
    
    TextView.setText() has left gravity by default. It's ugly in statusbar
    
    Change-Id: I7fac570597e925c47df45b788823bd959c972234
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    
    TrafficSB: Pass the animate bool
    
    - this should fix the indicator acting wonky according to this change
      introduced here
    
    Refer:
    https://github.com/ArrowOS/android_frameworks_base/commit/620ea2c7550ea8817ae055dbf983bf5af525d6a4#diff-b4e0e9525d5c2d3dcd9c31861671714d
    
    Change-Id: I169f8586959a57e1b2b62a241800e107b5092902
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    
    Bring back traffic indicator for Notch Devices !!!
    
    spezi77: Adapted for the reversed toggle to show arrows in statusbar network traffic indicators
    
    Change-Id: I011a142bdeba1557cc08f483a7cc3dd0423240c5
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    
    NetworkTraffic: Allow user to choose positioning [1/2]
    
    SDB - Bring it for PiA Devices aka Notch Devices
    ** Cleanup Codes to make it easy for noobs like me
    
    This allows the user to choose whether or not they want to see the traffic indicator in status bar or in the expanded header
    
    Change-Id: Ifff10bcccde91ea90f7bfe85acfe70ffc54a9418
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    
    Avoid casting network traffic to StatusBarIconView to prevent crash
    
    Change-Id: I5e32b57ddfecf80390eb14429ab3f7d510b10dbd
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    
    NetworkTraffic: Fix Net Text Getting Cut at DPI Changes
    
    Change-Id: I5d2064645b3e02d7f55ace5e294d5bf2fcf4d35f
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    
    NetworkTraffic: Set default font size of Single Traffic to 42dp.
    
    Change-Id: I8724a97688d1e66aafd77a9e8a07e1d9db59b486
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    
    [SQUASHED] Add Combined & Dynamic Network Traffic Mode [1/2]
    
    spezi77 edits:
    -Cleanup & update both NetworkTraffic classes
    
    Squashed in:
    -NetworkTraffic: Add combined option [1/2]
    -NetworkTraffic: Add Dynamic Mode [1/2] allow Combined mode to follow Dynamic mode activity arrows
    
    Change-Id: I85a48e3e17cd38a792f4d1bbed3367901f709d4c
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Joshua Primero <jprimero155@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4776,6 +4776,24 @@ public final class Settings {
          */
         public static final String GESTURE_DRAW_S = "gesture_draw_s";
 
+	/**
+	 * Wheter to show network traffic indicator in statusbar
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
+
+        /**
+         * Network traffic inactivity threshold (default is 1 kBs)
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
+
+	/**
+         * Whether to disable showing arrows in statusbar network traffic indicators
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_ARROW = "network_traffic_arrow";
+
         /**
          * Controls whether draw arrow left gesture is enabled.
          * @hide
@@ -5134,6 +5152,9 @@ public final class Settings {
             PRIVATE_SETTINGS.add(CAMERA_SLEEP_ON_RELEASE);
             PRIVATE_SETTINGS.add(CAMERA_LAUNCH);
             PRIVATE_SETTINGS.add(NAVIGATION_BAR_MODE_OVERLAY);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_STATE);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD);
+	        PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_ARROW);
         }
 
         /**
